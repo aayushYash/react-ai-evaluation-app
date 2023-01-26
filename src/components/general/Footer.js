36965d8829591c9c664c28bbef453493c377926c
subham kumar singh <shubhamrajni065@gmail.com>
2023-01-26 21:34:13 +0530

    check landing page

--- src/components/general/Footer.js
+++ src/components/general/Footer.js
@@ -5,20 +5,16 @@ export default function Footer() {
   return (
     <div className="footer">
       <div className="leftFooter">
-        <h2 className="title font-mono text-3xl font-thin">AI EVALUATOR</h2>
+        <h2 className="title ">AI EVALUATOR</h2>
 
         <div className="footer-address">
-          <p className=" px-2 font-bold">
-            Birla Institute of Technology, Lalpur
-          </p>
+          <p className="">Birla Institute of Technology, Lalpur</p>
           <p>+91-9374738384</p>
-          <p className="text-sm font-semibold px-2">
-            Mail: aievaluater.23@gmail.com
-          </p>
+          <p className="">Mail: aievaluater.23@gmail.com</p>
         </div>
       </div>
       <div className="rightFooter">
-        <p>Made with</p>
+        <p>Made with ♥️.</p>
       </div>
     </div>
   );
--- src/screens/LandingScreen.js
+++ src/screens/LandingScreen.js
@@ -7,57 +7,101 @@ export default function LandingScreen() {
   return (
     <div className="">
       <Header />
-      <div className="flex justify-between items-center flex-col md:flex-row">
-        <div className="w-full md:w-[50%]">
-          <img src="/images/show.png" alt="home_image" />
-        </div>
-        <div className=" w-full md:w-[50%] p-3 space-y-6">
-          <h1 className="p-2 text-5xl font-thin">AI Evaluation</h1>
-          <p className=" w-[80%] text-xs font-bold ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-            perspiciatis doloremque est pariatur itaque ullam optio culpa
-            consectetur qui sunt! Molestias earum perferendis sint sit tempora
-            consectetur. Eius, suscipit tenetur.
-          </p>
-          <div className="mt-2 flex justify-start items-center">
-            <Button text="Explore" />
-            <img className="w-8 " src="/images/arrow.gif" alt="" />
+      <div className=" flex justify-center  max-w-[1300px] my-10 bg-white mx-auto items-center flex-col">
+        <div className="flex justify-center md:justify-between flex-col md:flex-row  py-10 md:p-10 items-center w-full ">
+          <div className="flex justify-center items-center w-full md:w-[50%]">
+            <img
+              className=""
+              width={300}
+              height={200}
+              src="/images/ai banner.gif"
+              alt="ai_image"
+            />
+          </div>
+          <div className="w-full md:w-[50%] flex justify-center  flex-col p-5 space-y-10">
+            <h1 className=" text-5xl  font-bold text-purple-800">
+              Application Name
+            </h1>
+            <p className=" text-sm  font-mono text-gray-500  ">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Consequuntur dolorum eius est odit architecto culpa, cupiditate
+              dolorem harum ex quibusdam earum impedit iste iure facere quos
+              quae, eaque et quam.
+            </p>
+            <button className="bg-purple-800  w-[120px] text-sm px-1 py-2 flex justify-center items-center shadow-lg rounded-md text-white">
+              Explore{" "}
+              <img
+                className="ml-3"
+                width={20}
+                height={20}
+                src="/images/arrow1.gif"
+                alt=".."
+              />
+            </button>
           </div>
         </div>
-      </div>
-      <div className="flex justify-between items-center flex-col md:flex-row">
-        <div className="w-full md:w-[50%]">
-          <img src="/images/show.png" alt="home_image" />
-        </div>
-        <div className=" w-full md:w-[50%] p-3 space-y-6">
-          <h1 className="p-2 text-5xl font-thin">AI Evaluation</h1>
-          <p className=" w-[80%] text-xs font-bold md:text-left text-center">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-            perspiciatis doloremque est pariatur itaque ullam optio culpa
-            consectetur qui sunt! Molestias earum perferendis sint sit tempora
-            consectetur. Eius, suscipit tenetur.
-          </p>
-          <div className="mt-2 flex justify-start items-center">
-            <Button text="Explore" />
-            <img className="w-8 " src="/images/arrow.gif" alt="" />
+        <div className="flex justify-center md:justify-between flex-col md:flex-row   items-center w-full ">
+          <div className="w-full md:w-[60%] flex justify-center  flex-col  py-10 md:p-10 space-y-10">
+            <h1 className=" text-5xl px-4 font-bold text-purple-800">
+              Application Name
+            </h1>
+            <p className=" text-gray-500 text-sm font-mono px-4 ">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Consequuntur dolorum eius est odit architecto culpa, cupiditate
+              dolorem harum ex quibusdam earum impedit iste iure facere quos
+              quae, eaque et quam.
+            </p>
+            <button className="bg-purple-800 ml-4  w-[120px] text-sm px-1 py-2 flex justify-center items-center shadow-lg rounded-md text-white">
+              Explore{" "}
+              <img
+                className="ml-3"
+                width={20}
+                height={20}
+                src="/images/arrow1.gif"
+                alt=".."
+              />
+            </button>
+          </div>
+          <div className="flex justify-center items-center w-[50%]">
+            <img
+              className=""
+              width={500}
+              height={500}
+              src="/images/b4.gif"
+              alt="ai_image"
+            />
           </div>
         </div>
-      </div>
-      <div className="flex justify-between items-center flex-col md:flex-row">
-        <div className="w-full md:w-[50%]">
-          <img src="/images/show.png" alt="home_image" />
-        </div>
-        <div className=" w-full md:w-[50%] p-3 space-y-6">
-          <h1 className="p-2 text-5xl font-thin">AI Evaluation</h1>
-          <p className=" w-[80%] text-xs font-bold md:text-left text-center">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-            perspiciatis doloremque est pariatur itaque ullam optio culpa
-            consectetur qui sunt! Molestias earum perferendis sint sit tempora
-            consectetur. Eius, suscipit tenetur.
-          </p>
-          <div className="mt-2 flex justify-start items-center">
-            <Button text="Explore" />
-            <img className="w-8 " src="/images/arrow.gif" alt="" />
+        <div className="flex justify-center  h-full md:justify-between md:flex-row flex-col  py-10 md:p-10 items-center w-full ">
+          <div className="flex justify-center items-center w-full md:w-[50%]">
+            <img
+              className=""
+              width={400}
+              height={200}
+              src="/images/b1.gif"
+              alt="ai_image"
+            />
+          </div>
+          <div className=" md:w-[50%] flex justify-center flex-col p-5 space-y-10">
+            <h1 className=" text-5xl font-bold text-purple-800">
+              Application Name
+            </h1>
+            <p className=" text-sm font-mono text-gray-500 ">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Consequuntur dolorum eius est odit architecto culpa, cupiditate
+              dolorem harum ex quibusdam earum impedit iste iure facere quos
+              quae, eaque et quam.
+            </p>
+            <button className="bg-purple-800  w-[120px] text-sm px-1 py-2 flex justify-center items-center shadow-lg rounded-md text-white">
+              Explore{" "}
+              <img
+                className="ml-3"
+                width={20}
+                height={20}
+                src="/images/arrow1.gif"
+                alt=".."
+              />
+            </button>
           </div>
         </div>
       </div>
