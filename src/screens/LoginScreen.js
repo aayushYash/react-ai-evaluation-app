67ad8d89a378b598dc8cfe4246c3a9a7599095d9
aayushYash <aayash2002@gmail.com>
2023-01-30 18:49:58 +0530

    minor updates in login.js

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Player } from '@lottiefiles/react-lottie-player';
 
 import Footer from '../components/general/Footer'
@@ -13,25 +13,35 @@ import './LoginScreen.css'
 export default function LoginScreen() {
 
     const [emailId, setEmailId] = useState('');
+    const [password, setPassword] = useState('');
 
 
     const ForgotHandler = () => {
         console.log('click forgot handler')
     }
 
-    const OnChangeHandler = (e) => {
+    const onEmailChange = (e) => {
         setEmailId(e.target.value)
     }
 
-    const radioChangeHandler = (e) => {
-        console.log(e.target.value)
+    const onPasswordChange = (e) => {
+        setPassword(e.target.val)
     }
 
     const ToggleHandler = (e) => {
         console.log(e.target.value)
     }
 
-    console.log(emailId)
+    // firebase functionality below 👇.
+
+
+    const LoginHandler = () => {
+        // implement firebase login with password
+    }
+
+    const LoginWithGoogle = () => {
+        console.log('Login with google');
+    }
 
   return (
     <div className='loginPage'>
@@ -50,15 +60,15 @@ export default function LoginScreen() {
             <Form>
                 <ToggleButton group='typeofuser' changeHandler={ToggleHandler} />
                 <span style={{'fontWeight': 700,'margin' : '10px 0'}}>LOGIN</span>
-                <GoogleButton text='Continue with' />
+                <GoogleButton text='Continue with' onClickHandler={LoginWithGoogle} />
                 <span>
                     <legend>OR</legend>
                 </span>
-                <InputText val={emailId} onchange={OnChangeHandler} icon='envelope' type={'text'} />
-                <InputText val={emailId} onchange={OnChangeHandler} icon='lock' type={'password'} />
+                <InputText val={emailId} onchange={onEmailChange} icon='envelope' type={'text'} />
+                <InputText val={password} onchange={onPasswordChange} icon='lock' type={'password'} />
 
                 <p onClick={ForgotHandler} style={{'color': '#1947FF' , 'cursor': 'pointer', 'width': '300px'}} >Forgot Password ?</p>
-                <Button text='Login' icon='caret-right' />
+                <Button text='Login' icon='caret-right' onclick={LoginHandler} />
             </Form>
             </div>
         </div>
