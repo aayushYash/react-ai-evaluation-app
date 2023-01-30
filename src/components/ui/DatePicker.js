9f8c21b37a62fd6170b1b7a94380ed220d5134d3
subham kumar singh <shubhamrajni065@gmail.com>
2023-01-30 19:04:37 +0530

    register page 2

--- src/components/ui/DatePicker.js
+++ src/components/ui/DatePicker.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function DatePicker({ value, onChange }) {
   return (
-    <div className="flex cursor-pointer justify-center -ml-16 py-2 px-1 w-[250px]  items-center space-x-3">
+    <div className="flex cursor-pointer mt-3 justify-center -ml-8 py-2 px-1 w-[250px]  items-center space-x-3">
       <FontAwesomeIcon icon="fa-solid fa-calendar" />
       <div className=" relative w-[200px] border h-[30px] border-gray-500 rounded-md">
         <input
