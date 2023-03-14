8222e767c6ee505937fa2ff62a30e27e667bf2e4
subham kumar singh <shubhamrajni065@gmail.com>
2023-03-14 14:09:08 +0530

    studentDashboard ...

--- src/screens/StudentDashboard.js
+++ src/screens/StudentDashboard.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import Header from '../components/general/Header'
+import React from "react";
+import Header from "../components/general/Header";
 
 export default function StudentDashboard() {
   return (
     <div>
-        <Header />
+      <Header />
     </div>
-  )
+  );
 }
